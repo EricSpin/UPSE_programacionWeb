70affbdcc68eb3b08dcb9409b3b1e5e1a5bc12f0
ERICK SUAREZ <ssping5@gmail.com>
2016-04-24 15:59:19 -0500

    Arreglo de menu en Jquery

--- js/app.js
+++ js/app.js
@@ -45,3 +45,39 @@ $("#galeria li a").click(function(event){
 $overlay.click(function(){
 	$overlay.hide();
 });
+
+//creamos el menu lateral 
+var $select = $("<select></select>");
+$("#menu").append($select);
+
+//Recorrer el menu
+$("#menu a").each(function(){
+	var $anchor = $(this);
+	//Crear una opcion para el select por cada elemento del menu
+	var $option = $("<option></option>");
+
+	//Obtener el valor de la opcion del atributo href
+	$option.val($anchor.attr("href"));
+
+	//Obtener el text de cada opcion
+	$option.text($anchor.text());
+
+	//agregar la opcion al select
+	$select.append($option);
+
+	//Obtener si el link es la pagina actual
+
+	if($(this).parent().hasClass("selected"))
+	{
+		$option.prop("selected", true);
+	}
+});
+
+//boton de seleccion
+var $button = $("<button>Go</button>");
+$("#menu").append($button);
+
+$button.click(function(){
+	//Toma el valor de la opcion seleccionada
+	window.location = $select.val();
+});
